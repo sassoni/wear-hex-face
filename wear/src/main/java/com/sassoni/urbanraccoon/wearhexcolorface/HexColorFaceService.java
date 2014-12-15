e566c1471f81b2e9fda07747eeb0d902529d1ca7
sassoni <chrysanthi.n@gmail.com>
2014-12-14 23:05:32 -0500

    Empty face service

---
+++ wear/src/main/java/com/sassoni/urbanraccoon/wearhexcolorface/HexColorFaceService.java
@@ -0,0 +1,7 @@
+package com.sassoni.urbanraccoon.wearhexcolorface;
+
+import android.support.wearable.watchface.CanvasWatchFaceService;
+
+public class HexColorFaceService extends CanvasWatchFaceService {
+
+}
