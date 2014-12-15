77922423f635c0aba8a444e79dcaa99f3a47d140
sassoni <chrysanthi.n@gmail.com>
2014-12-15 00:55:39 -0500

    Simple drawing, configuration on manifest to show in the menu, adding test drawables.

--- wear/src/main/java/com/sassoni/urbanraccoon/wearhexcolorface/HexColorFaceService.java
+++ wear/src/main/java/com/sassoni/urbanraccoon/wearhexcolorface/HexColorFaceService.java
@@ -1,7 +1,47 @@
 package com.sassoni.urbanraccoon.wearhexcolorface;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Rect;
 import android.support.wearable.watchface.CanvasWatchFaceService;
+import android.support.wearable.watchface.WatchFaceStyle;
+import android.text.DynamicLayout;
+import android.text.TextPaint;
+import android.view.SurfaceHolder;
 
 public class HexColorFaceService extends CanvasWatchFaceService {
+
+    @Override
+    public Engine onCreateEngine() {
+        return new Engine();
+    }
+
+    public class Engine extends CanvasWatchFaceService.Engine {
+        static final int BACKGROUND_COLOR = Color.BLACK;
+        static final int FOREGROUND_COLOR = Color.WHITE;
+        static final int TEXT_SIZE = 25;
+
+        final TextPaint mTextPaint = new TextPaint();
+
+        @Override
+        public void onCreate(SurfaceHolder holder) {
+            super.onCreate(holder);
+
+            setWatchFaceStyle(new WatchFaceStyle.Builder(HexColorFaceService.this)
+                    .setCardPeekMode(WatchFaceStyle.PEEK_MODE_VARIABLE)
+                    .setBackgroundVisibility(WatchFaceStyle.BACKGROUND_VISIBILITY_INTERRUPTIVE)
+                    .setShowSystemUiTime(false)
+                    .build());
+
+            mTextPaint.setColor(FOREGROUND_COLOR);
+            mTextPaint.setTextSize(TEXT_SIZE);
+        }
+
+        @Override
+        public void onDraw(Canvas canvas, Rect bounds) {
+            canvas.drawColor(BACKGROUND_COLOR);
+            canvas.drawText("Testing", 10, 10, mTextPaint);
+        }
+    }
 
 }
